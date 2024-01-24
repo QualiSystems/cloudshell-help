e08c06676a5b5c21b971eb7d7c35bd11ac3f1b3b
Tomer Admon <Tomer.a@qualisystems.com>
2024-01-24 15:29:56 +0200

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/QualiSystems/cloudshell-help/tree/master/',
+            'https://github.com/QualiSystems/cloudshell-help/tree/main/',
 
         },
         theme: {
