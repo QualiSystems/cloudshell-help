3683ad2974b8f31113ae5a071e50609d59ee8045
kalsky <yanivka@gmail.com>
2024-08-19 11:16:38 -0500

    add sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,6 +72,17 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        sitemap: {
+          lastmod: 'date',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+        },
       }),
     ],
   ],
