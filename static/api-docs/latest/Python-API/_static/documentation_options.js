d8e466bc060e9091d38f9a35c78e31d22954f456
Yaniv Kalsky <yaniv.k@quali.com>
2026-06-01 09:43:57 -0500

    update api docs

--- static/api-docs/latest/Python-API/_static/documentation_options.js
+++ static/api-docs/latest/Python-API/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2024.1.0.187973',
+    VERSION: '2026.1.0.189799',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
